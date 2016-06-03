07a8d2944b94ad0239955e1b692b1019b7174696
seyma06 <sakay@outlook.com>
2016-06-03 11:03:35 +0300

    source

---
+++ src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.example.helloworld;
+
+/**
+ * Created by Administrator on 31.05.2016.
+ */
+public class HelloWorld {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+}
+
+
+
